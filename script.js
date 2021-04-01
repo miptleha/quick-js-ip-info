b1855e0bdc90334caacc77163b551428d6438704
Aleksey Obukhov <miptleha@gmail.com>
2021-04-01 11:48:56 +0300

    fix errors

--- script.js
+++ script.js
@@ -1,50 +1,96 @@
-// This example requires the Places library. Include the libraries=places
-// parameter when you first load the API. For example:
-// <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBIwzALxUPNbatRBj3Xi1Uhp0fFzwWNBkE&libraries=places">
+//https://developers.google.com/maps/documentation/javascript/examples/map-latlng-literal?hl=ru#maps_map_latlng_literal-javascript
 let map;
-let service;
-let infowindow;
 
-function initMap(x, y) {
-  const sydney = new google.maps.LatLng();
-  infowindow = new google.maps.InfoWindow();
-  map = new google.maps.Map(document.getElementById("map"), {
-    center: sydney,
-    zoom: 15,
-  });
-  const request = {
-    query: "Museum of Contemporary Art Australia",
-    fields: ["name", "geometry"],
+function initMap(lat, lng) {
+  const mapOptions = {
+    zoom: 8,
+    center: { lat: lat, lng: lng },
   };
-  service = new google.maps.places.PlacesService(map);
-  service.findPlaceFromQuery(request, (results, status) => {
-    if (status === google.maps.places.PlacesServiceStatus.OK && results) {
-      for (let i = 0; i < results.length; i++) {
-        createMarker(results[i]);
-      }
-      map.setCenter(results[0].geometry.location);
-    }
-  });
-}
-
-function createMarker(place) {
-  if (!place.geometry || !place.geometry.location) return;
+  map = new google.maps.Map(document.getElementById("map"), mapOptions);
   const marker = new google.maps.Marker({
-    map,
-    position: place.geometry.location,
+    // The below line is equivalent to writing:
+    // position: new google.maps.LatLng(-34.397, 150.644)
+    position: { lat: lat, lng: lng },
+    map: map,
+  });
+  // You can use a LatLng literal in place of a google.maps.LatLng object when
+  // creating the Marker object. Once the Marker object is instantiated, its
+  // position will be available as a google.maps.LatLng object. In this case,
+  // we retrieve the marker's position using the
+  // google.maps.LatLng.getPosition() method.
+  const infowindow = new google.maps.InfoWindow({
+    content: "<p>Marker Location:" + marker.getPosition() + "</p>",
   });
   google.maps.event.addListener(marker, "click", () => {
-    infowindow.setContent(place.name || "");
-    infowindow.open(map);
+    infowindow.open(map, marker);
   });
 }
 
-function showMap() {
-    $('#map').css({height: "100%"});
-    initMap(-33.867, 151.195);
+function err(msg) {
+  var e = document.getElementById("error");
+  e.innerHTML = msg;
 }
 
 $('#check').click(function() {
-    $('#map').css({height: "100%"});
-    initMap(-33.867, 151.195);
+  err('');
+
+  if (!window.navigator.onLine) {
+    err('Internet connection required!');
+    return;
+  }
+  
+  var ip = $('#ip').val();
+  if (!/^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/.test(ip)) {
+    err('Invalid IP Address!')
+    return;
+  }
+  $.get("http://free.ipwhois.io/xml/" + ip, function(res) {
+    var xmlDoc = res;
+    var str = new XMLSerializer().serializeToString(xmlDoc.documentElement);
+    console.log(str);
+
+    var r = new RegExp("<continent>(.*?)</continent>(.*?)<country>(.*?)</country>(.*?)<country_flag>(.*?)</country_flag>(.*?)<region>(.*?)</region>(.*?)<city>(.*?)</city>(.*?)<currency>(.*?)</currency>");
+    var m = r.exec(str);
+    var continent = '-', country = '-', flag = '-', region='-', city='-', cur='-';
+    if (m != null) {
+      continent = m[1];
+      country = m[3];
+      flag = m[5];
+      region = m[7];
+      city = m[9];
+      cur = m[11];
+    }
+    else {
+      err('Unknown location!');
+    }
+    $('#continent').text(continent);
+    $('#country').text(country);
+    $('#flag').attr('src', flag);
+    $('#region').text(region);
+    $('#city').text(city);
+    $('#cur').text(cur);
+
+    var rorg = new RegExp("<org>(.*?)</org>");
+    var morg = rorg.exec(str);
+    if (morg != null) {
+      $('#org').text(morg[1]);
+    }
+    else {
+      $('#org').text('-');
+    }
+
+    var rloc = new RegExp("<latitude>(.*?)</latitude>(.*?)<longitude>(.*?)</longitude>");
+    var mloc = rloc.exec(str);
+    if (mloc != null) {
+      var lat = parseFloat(mloc[1]);
+      var lng = parseFloat(mloc[3]);
+
+      $('#map').css({height: "100%"});
+      initMap(lat, lng);
+    }
+    else {
+      err('Unknown location!');
+      $('#map').css({height: "0%"});
+    }
+  });
 });
